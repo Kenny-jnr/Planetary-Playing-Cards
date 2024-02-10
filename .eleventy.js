e0b9f3bfaa47aab495b9a1384b1433212cd0010f
Kenny Umobi <122109136+Kenny-jnr@users.noreply.github.com>
2024-02-10 22:04:52 +0000

    made playing cards

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("planets", function(name, name2, AU) {
+    return `<div class="planet">
+    <div class="circle ${name2}"></div>
+    <h2 class="name">${name}</h2>
+    <p>${AU}</p>
+  </div>`
+  })
 };
